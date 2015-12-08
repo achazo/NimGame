8d4ea52cc630264a821ba147da95e4496e6525c4
acminaux <acminaux@gmail.com>
2015-12-08 11:54:24 +0100

    Refactoring

--- src/main/java/fr/ut7/games/NimGame.java
+++ src/main/java/fr/ut7/games/NimGame.java
@@ -3,27 +3,24 @@ package fr.ut7.games;
 import java.util.ArrayList;
 
 public class NimGame {
+    public static final int PLAYER_NUMBER = 2;
+    public static final String NO_WINNER = "";
+
     private int stickNumber;
     private String lastPlayer;
     private ArrayList<String> players;
 
     public NimGame(ArrayList<String> initialPlayers, int initialStickNumber) throws NoPlayerException {
         stickNumber = initialStickNumber;
-        if (initialPlayers.size() != 2) {
+        if (notEnoughPlayer(initialPlayers)) {
             throw new NoPlayerException();
         }
         players = initialPlayers;
     }
 
     public String winner() {
-        if (stickNumber <= 0) {
-            if (lastPlayer.equals(players.get(0))) {
-                return players.get(1);
-            } else if (lastPlayer.equals(players.get(1))) {
-                return players.get(0);
-            }
-        }
-        return "";
+        if (noMoreStick()) return getSecondToLast();
+        return NO_WINNER;
     }
 
     public void play(String player, int stickToRemove) {
@@ -38,5 +35,20 @@ public class NimGame {
             return false;
         }
         return true;
+    }
+
+    private String getSecondToLast() {
+        if (lastPlayer.equals(players.get(0))) {
+            return players.get(1);
+        }
+        return players.get(0);
+    }
+
+    private boolean noMoreStick() {
+        return stickNumber <= 0;
+    }
+
+    private boolean notEnoughPlayer(ArrayList<String> players) {
+        return players.size() != PLAYER_NUMBER;
     }
 }
